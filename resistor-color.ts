b82fa2990f30c06c81e5b3fdd171e384d2b50c59
Victor Fernandez <vfernandezInnovation>
2022-03-29 09:44:00 +0200

    Ex1 Completado

--- resistor-color.ts
+++ resistor-color.ts
@@ -1,5 +1,26 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
+export const colorCode = (color : string) => {
+  const colors : String[] = 
+  ['black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'];
+  return colors.indexOf(color.toLocaleLowerCase())
 }
 
-export const COLORS = undefined
+export const COLORS = 
+['black',
+'brown',
+'red',
+'orange',
+'yellow',
+'green',
+'blue',
+'violet',
+'grey',
+'white'];
